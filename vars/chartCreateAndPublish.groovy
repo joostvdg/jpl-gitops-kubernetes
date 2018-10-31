483185afec3c48c15e1a3f060094cbea2bff7c7e
Joost van der Griendt <joostvdg@gmail.com>
2018-10-31 14:06:37 +0100

    allow more customization

--- vars/chartCreateAndPublish.groovy
+++ vars/chartCreateAndPublish.groovy
@@ -1,9 +1,9 @@
-def call(chartName, chartVersion, repoUrl, repoCredentialsId, containerName = 'helm') {
+def call(chartName, chartVersion, repoUrl, repoCredentialsId, helmFolder ='helm/', containerName = 'helm') {
     container("${containerName}") {
         // TODO: do something with helm lint errors
         withCredentials([usernameColonPassword(credentialsId: repoCredentialsId, variable: 'CREDS')]) {
-            sh """helm lint helm/cat-nip
-            helm package helm/cat-nip
+            sh """helm lint ${helmFolder}${chartName}
+            helm package ${helmFolder}${chartName}
             curl --insecure -u ${CREDS} --data-binary \"@${chartName}-${chartVersion}.tgz\" ${repoUrl}/api/charts
             """
         }
