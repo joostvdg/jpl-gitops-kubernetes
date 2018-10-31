810d1006b3d2f0bdb1fd4fcff7ae7b273e4d9f51
Joost van der Griendt <joostvdg@gmail.com>
2018-10-31 13:27:09 +0100

    minor tweaks

--- vars/kanikoBuild.groovy
+++ vars/kanikoBuild.groovy
@@ -1,8 +1,8 @@
-def call(dockerfile, destination, containerName = 'kaniko') {
+def call(dockerfile, destination, params = '', containerName = 'kaniko') {
     container(name: containerName, shell: '/busybox/sh') {
-        withEnv(['PATH+EXTRA=/busybox', "FILE=${dockerfile}", "DEST=${destination}"]) {
+        withEnv(['PATH+EXTRA=/busybox', "FILE=${dockerfile}", "DEST=${destination}", "PARAMS=$params"]) {
             sh '''#!/busybox/sh
-                /kaniko/executor --cleanup -f `pwd`/${FILE} -c `pwd` --insecure-skip-tls-verify --cache=true --destination=${DEST}
+                /kaniko/executor --cleanup -f `pwd`/${FILE} -c `pwd` --cache=true --destination=${DEST} ${PARAMS}
             '''
         }
     }
